56a5be927d5bcf0bce3b69590dcf34263cd2050f
tamar-dev <tamarsharabi123@gmail.com>
2026-07-08 00:26:31 +0300

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+
+        const res = strs.reduce(
+        (accumulator, currentValue) => accumulator+currentValue.length+'#' + currentValue,"");
+    return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let sp = 0, len =0, res =[];
+        while (str.length > 0) {
+            sp = str.indexOf('#');
+            if(sp == -1) {
+                res.push(str);
+                break;
+            }
+            len = Number.parseInt(str.slice(0, sp));
+            res.push(str.slice(sp + 1, sp + 1 + len));
+            str = str.slice(sp + 1 + len);
+
+        }
+        return res;
+    }
+
+}
